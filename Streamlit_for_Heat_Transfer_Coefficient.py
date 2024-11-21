72872a0e64289f18e5d0f1ecdd9b65840e76236c
Mohab <82895440+Mohab-12@users.noreply.github.com>
2024-11-21 18:45:52 +0200

    Update Streamlit_for_Heat_Transfer_Coefficient.py

--- Streamlit_for_Heat_Transfer_Coefficient.py
+++ Streamlit_for_Heat_Transfer_Coefficient.py
@@ -44,7 +44,7 @@ scaler_y = MinMaxScaler()
 Y = scaler_y.fit_transform(y.reshape(-1, 1))
 
 # Load trained model
-model = load_model('best_model0.keras')  # Ensure you save the trained model with this name
+model = load_model('best_model.keras')  # Ensure you save the trained model with this name
 
 # Streamlit App
 st.title("Heat Transfer Coefficient Prediction")
