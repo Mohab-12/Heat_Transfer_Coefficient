a170d431d166b380e9f17f5382f868f2c8c894dd
Mohab <82895440+Mohab-12@users.noreply.github.com>
2024-11-28 14:05:22 +0200

    Update Streamlit_for_Heat_Transfer_Coefficient.py

--- Streamlit_for_Heat_Transfer_Coefficient.py
+++ Streamlit_for_Heat_Transfer_Coefficient.py
@@ -56,22 +56,22 @@ st.title("Heat Transfer Coefficient Prediction")
   st.subheader("Enter Input Features")
 
   # Input form
-  form = st.form(key="custom_input_form")
-  col1, col2 = form.columns(2)
+  # form = st.form(key="custom_input_form")
+  # col1, col2 = form.columns(2)
 
-  with col1:
-      mixture_temp = form.number_input("Mixture Temperature (°C)", value=25.0)
-      mass_fraction = form.number_input("Mass Fraction", value=0.5)
-      dewpoint = form.number_input("Dewpoint", value=10.0)
-      mixture_flow_rate = form.number_input("Mixture Flow Rate (kg/h)", value=100.0)
-      cooling_flow_rate = form.number_input("Cooling Water Flow Rate (l/h)", value=50.0)
-      cooling_temp = form.number_input("Cooling Water Inlet Temp (°C)", value=15.0)
+  # with col1:
+    mixture_temp = form.number_input("Mixture Temperature (°C)", value=25.0)
+    mass_fraction = form.number_input("Mass Fraction", value=0.5)
+    dewpoint = form.number_input("Dewpoint", value=10.0)
+    mixture_flow_rate = form.number_input("Mixture Flow Rate (kg/h)", value=100.0)
+    cooling_flow_rate = form.number_input("Cooling Water Flow Rate (l/h)", value=50.0)
+    cooling_temp = form.number_input("Cooling Water Inlet Temp (°C)", value=15.0)
 
-  with col2:
-      specific_heat = form.number_input("Specific Heat (kJ/kg K)", value=1.0, format="%.9f")
-      viscosity = form.number_input("Viscosity (μPa s)", value=10.00, format="%.9f")
-      thermal_conductivity = form.number_input("Thermal Conductivity (W/m K)", value=0.1, format="%.9f")
-      latent_heat = form.number_input("Latent Heat of Vaporization (kJ/kg)", value=2200.0)
+  # with col2:
+    specific_heat = form.number_input("Specific Heat (kJ/kg K)", value=1.0, format="%.9f")
+    viscosity = form.number_input("Viscosity (μPa s)", value=10.00, format="%.9f")
+    thermal_conductivity = form.number_input("Thermal Conductivity (W/m K)", value=0.1, format="%.9f")
+    latent_heat = form.number_input("Latent Heat of Vaporization (kJ/kg)", value=2200.0)
 
   submit_button = form.form_submit_button(label="Predict")
 
